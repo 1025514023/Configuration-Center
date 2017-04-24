50eabf264fe12da19c50fc6231df54179562c70f
wangxizhong <1025514023@qq.com>
2017-04-24 18:04:14 +0800

    commit add controller

---
+++ src/main/java/xzfm/core/controller/ServerController.java
@@ -0,0 +1,10 @@
+package xzfm.core.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangxizhong on 2017/4/24.
+ */
+@RestController
+public class ServerController {
+}
---
+++ src/main/java/xzfm/core/domain/Configuration_Center.java
@@ -0,0 +1,7 @@
+package xzfm.core.domain;
+
+/**
+ * Created by wangxizhong on 2017/4/24.
+ */
+public class Configuration_Center {
+}
